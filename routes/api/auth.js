bb31b2a0cd208c5bf687da3e1c10dd48d3102480
Nijarudeen <nijarr2020@gmail.com>
2020-04-08 23:11:36 +0530

    user authorized

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const User = require('../../models/Users');
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+    res.json(user)
+  } catch (error) {
+   console.error(error.message);
+   res.status(500).send('Server Error') 
+  }
+})
 
 module.exports = router;
