bb0eab9bbf8920fc5860158fdaa135afe2142246
Nijarudeen <nijarr2020@gmail.com>
2020-04-08 20:34:50 +0530

    user modal added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    // See if user exists
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+ 
+    res.send("User route");
+  }
+);
 
 module.exports = router;
