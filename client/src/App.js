78731c1af1be9d18d7d0adcc7354c5edc5e3f1ba
Nijarudeen <nijarr2020@gmail.com>
2020-04-30 19:21:47 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h2>App</h2>
+    </Fragment>
+  );
+};
+
+export default App;
