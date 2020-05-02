56891a722e6185536e6daeae32b95e5135565a31
Nijarudeen <nijarr2020@gmail.com>
2020-05-02 16:57:12 +0530

    delete post

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/post/Posts";
 
 
 if (localStorage.token) {
@@ -62,6 +63,11 @@ const App = () => {
                 exact
                 path="/add-education"
                 component={AddEducation}
+              />
+              <PrivateRoute
+                exact
+                path="/posts"
+                component={Posts}
               />
             </Switch>
           </section>
