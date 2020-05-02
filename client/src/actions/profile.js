780c0704516e7734e89145f8c6a9bc292b629423
Nijarudeen <nijarr2020@gmail.com>
2020-05-02 15:59:46 +0530

    profile displayed

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -70,8 +70,8 @@ export const getProfileById = (userId) => async (dispatch) => {
 };
 
 // Get getGihubRepos
-export const getGihubRepos = (username) => async (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE });
+export const getGithubRepos = (username) => async (dispatch) => {
+  //dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 
