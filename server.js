44d7718295f29bbd7cfab0724f8c9ff05e874541
Nijarudeen <nijarr2020@gmail.com>
2020-05-02 20:03:14 +0530

    deployment

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require('cors');
 const app = express();
-
+const path = require('path');
 // Connect DB
 connectDB();
 app.use(cors())
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running..."));
+//app.get("/", (req, res) => res.send("API running..."));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -17,6 +17,14 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 
+// Serve static assets in prod
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
